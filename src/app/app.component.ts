0d731d17e82409da589b1ccbfa8a352d9445e77b
emanawad14 <emanelnady2001@gmail.com>
2025-07-04 12:49:19 +0300

    firstCommit

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent  implements OnInit{
+  title = 'SouqakStore';
+
+
+ 
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
+}
