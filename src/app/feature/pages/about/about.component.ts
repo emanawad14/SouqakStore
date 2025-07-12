efbd1e98cd1485d438cdaa05455ad3a5738be8bb
emanawad14 <emanelnady2001@gmail.com>
2025-07-12 15:22:54 +0300

    DoneAbout

--- src/app/feature/pages/about/about.component.ts
+++ src/app/feature/pages/about/about.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { IconsComponent } from "../icons/icons.component";
+import { FeatureComponent } from "../feature/feature.component";
+import { ServicesComponent } from "../services/services.component";
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [IconsComponent, FeatureComponent, ServicesComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
