e540ab3c5d67dd94737ceefce5535060e7b66c1e
emanawad14 <emanelnady2001@gmail.com>
2025-07-27 19:21:34 +0300

    addWishList

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,5 +7,6 @@ export const routes: Routes = [
     {path:'about' , loadComponent:()=>import('./feature/pages/about/about.component').then((c)=>c.AboutComponent) , title:'about'},
     {path:'contact' , loadComponent:()=>import('./feature/pages/contact/contact.component').then((c)=>c.ContactComponent) , title:'contact'},
     {path:'cart' , loadComponent:()=>import('./feature/pages/cart/cart.component').then((c)=>c.CartComponent) , title:'cart'},
+    {path:'wishlist' , loadComponent:()=>import('./feature/pages/wishlist/wishlist.component').then((c)=>c.WishlistComponent) , title:'wishlist'},
     {path:'**' , loadComponent:()=>import('./feature/pages/notfound/notfound.component').then((c)=>c.NotfoundComponent) , title:'No Found'},
 ];
