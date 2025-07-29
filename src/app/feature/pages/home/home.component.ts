40afc256051aa836d5516f3c4d4c64712c821b77
AyaAshraf5 <yoyo13625@gmail.com>
2025-07-29 06:24:01 +0300

    first

--- src/app/feature/pages/home/home.component.ts
+++ src/app/feature/pages/home/home.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { NavComponent } from "../../../core/layouts/nav/nav.component";
-import { FootnavComponent } from "../../../core/layouts/footnav/footnav.component";
+import { NavComponent } from '../../../core/layouts/nav/nav.component';
+import { FootnavComponent } from '../../../core/layouts/footnav/footnav.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-home',
-  imports: [NavComponent, FootnavComponent],
+  imports: [NavComponent, FootnavComponent, HeaderComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
